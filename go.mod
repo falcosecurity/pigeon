3f814ebde6ac8a95c69daae1d87f96030443460c
Jason Dellaluce <jasondellaluce@gmail.com>
2023-01-24 15:51:28 +0000

    new: config
    Signed-off-by: Jason Dellaluce <jasondellaluce@gmail.com>

--- go.mod
+++ go.mod
@@ -13,4 +13,5 @@ require (
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/poiana/config.go
@@ -0,0 +1,32 @@
+package poiana
+
+import (
+	"io"
+
+	"gopkg.in/yaml.v3"
+)
+
+type GitHubRepoActions struct {
+	Variables []string `yaml:"variables"`
+	Secrets   []string `yaml:"secrets"`
+}
+
+type GitHubRepo struct {
+	Actions GitHubRepoActions `yaml:"actions"`
+}
+
+type GitHubOrg struct {
+	Repos map[string]GitHubRepo
+}
+
+type GithubConfig struct {
+	Orgs map[string]GitHubOrg `yaml:"orgs"`
+}
+
+func (e *GithubConfig) Decode(r io.Reader) error {
+	return yaml.NewDecoder(r).Decode(e)
+}
+
+func (e *GithubConfig) Encode(w io.Writer) error {
+	return yaml.NewEncoder(w).Encode(e)
+}
